31a1c4834a5353b89cdc811f1ec0948b84d382f4
Diego <diegoolliveira1999@gmail.com>
2021-02-24 12:45:48 -0300

    Injeção de dependências no pacote Apresentação, criação do arquivo config.properties

--- Apresentacao/src/main/java/br/edu/ifnmg/apresentacao/Console.java
+++ Apresentacao/src/main/java/br/edu/ifnmg/apresentacao/Console.java
@@ -5,20 +5,9 @@
  */
 package br.edu.ifnmg.apresentacao;
 
-import br.edu.ifnmg.logicaaplicacao.Cliente;
-import br.edu.ifnmg.logicaaplicacao.ClienteRepositorio;
-import br.edu.ifnmg.logicaaplicacao.Produto;
-import br.edu.ifnmg.logicaaplicacao.ProdutoRepositorio;
-import br.edu.ifnmg.logicaaplicacao.Transacao;
-import br.edu.ifnmg.logicaaplicacao.TransacaoItem;
+import br.edu.ifnmg.logicaaplicacao.RepositorioFactory;
 import br.edu.ifnmg.logicaaplicacao.TransacaoRepositorio;
-import br.edu.ifnmg.logicaaplicacao.TransacaoTipo;
-import br.edu.ifnmg.logicaaplicacao.Usuario;
 import br.edu.ifnmg.logicaaplicacao.UsuarioRepositorio;
-import br.edu.ifnmg.persistencia.ClienteDAO;
-import br.edu.ifnmg.persistencia.ProdutoDAO;
-import br.edu.ifnmg.persistencia.TransacaoDAO;
-import br.edu.ifnmg.persistencia.UsuarioDAO;
 
 /**
  *
@@ -30,67 +19,12 @@ public class Console {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         
-//        UsuarioRepositorio repo = new UsuarioDAO();
-//        
-//        Usuario u = new Usuario();
-//        u.setLogin("teste");
-//        u.setSenha("123");
-//        
-//        if(repo.Salvar(u)){
-//        
-//        System.out.println("Sucesso");
-//      
-//        }else{
-//    
-//        System.out.println("Falhou");
-//        }
-//        ClienteRepositorio repo = new ClienteDAO();
-//        Cliente c = new Cliente();
-//        c.setCpf("123455");
-//        c.setSexo(Sexo.Homem);
-//        c.setNome("Carlos");
-//        
-//        if(repo.Salvar(c)){
-//            System.out.println("Sucesso");
-//        }else{
-//            System.out.println("Falhou");
-//        }
-    
-//        ClienteRepositorio repo = new ClienteDAO();
-//        Cliente c = new Cliente();
-//        c.setCpf("123455");
-//        c.setSexo(Sexo.Homem);
-//        c.setNome("Carlos");
-//        
-//        if(repo.Salvar(c)){
-//            System.out.println("Sucesso");
-//        }else{
-//            System.out.println("Falhou");
-//        }
-        
-        ProdutoRepositorio repo = new ProdutoDAO();
-        Produto pa = repo.Abrir(101L);
-        Produto pb = repo.Abrir(151L);
-        
-        ClienteRepositorio repoC = new ClienteDAO();
-        Cliente c = repoC.Abrir(1L);
-        
-        UsuarioRepositorio repoU = new UsuarioDAO();
-        Usuario u = repoU.Abrir(201L);
-        
-        TransacaoRepositorio repoT = new TransacaoDAO();
-        Transacao t = new Transacao(c,TransacaoTipo.Venda,u);
-        
-        TransacaoItem ti = new TransacaoItem(pa,10,t);
-        TransacaoItem te = new TransacaoItem(pb,10,t);
-        
-        t.add(ti);
-        t.add(te);
-        
-        repoT.Salvar(t);
-        
+            UsuarioRepositorio repo = RepositorioFactory.getUsusarioRepositorio();
+            System.out.println(repo.Abrir(301L).getLogin());
+            
+            TransacaoRepositorio repoT = RepositorioFactory.getTransacaoRepositorio();
+            System.out.println(repoT.Abrir(251L).getValorTotal());
     }
    
     
---
+++ LogicaAplicacao/src/main/java/br/edu/ifnmg/logicaaplicacao/RepositorioFactory.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.logicaaplicacao;
+
+import java.io.Console;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author diego
+ */
+public class RepositorioFactory {
+    
+    private static Properties propriedades = new Properties();
+    private static UsuarioRepositorio usuario;
+    private static PessoaRepositorio pessoa;
+    private static TransacaoRepositorio transacao;
+    private static ClienteRepositorio cliente;
+    private static FornecedorRepositorio fornecedor;
+    private static ProdutoRepositorio produto;
+    
+    static{
+        FileReader leitorArquivo = null;
+            
+            try {
+                // TODO code application logic here
+                
+                File arquivoPropriedades = new File("config.properties");
+                leitorArquivo = new FileReader(arquivoPropriedades);
+                
+                propriedades.load(leitorArquivo);
+                
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Console.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Console.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                try {
+                    leitorArquivo.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(Console.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+    }
+    
+    private static Object getInstancia(String nomeclasse){
+        try {    
+            Class classe = Class.forName(nomeclasse);
+            Object instancia = classe.getDeclaredConstructor().newInstance();
+            return instancia;
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Console.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchMethodException ex) {
+            Logger.getLogger(Console.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(Console.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Console.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Console.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(Console.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvocationTargetException ex) {
+            Logger.getLogger(Console.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    public static UsuarioRepositorio getUsusarioRepositorio(){
+        if(usuario == null){
+            String nomeclasse = propriedades.getProperty("UsuarioRepositorio");
+            usuario = (UsuarioRepositorio) getInstancia(nomeclasse);
+        }
+        return usuario;
+    }
+    
+    public static PessoaRepositorio getPessoaRepositorio(){
+        if(pessoa == null){
+            String nomeclasse = propriedades.getProperty("PessoaRepositorio");
+            pessoa = (PessoaRepositorio) getInstancia(nomeclasse);
+        }
+        return pessoa;
+    }
+    
+    public static ClienteRepositorio getClienteRepositorio(){
+        if(cliente == null){
+            String nomeclasse = propriedades.getProperty("ClienteRepositorio");
+            cliente = (ClienteRepositorio) getInstancia(nomeclasse);
+        }
+        return cliente;
+    }
+    
+    public static FornecedorRepositorio getFornecedorRepositorio(){
+        if(fornecedor == null){
+            String nomeclasse = propriedades.getProperty("FornecedorRepositorio");
+            fornecedor = (FornecedorRepositorio) getInstancia(nomeclasse);
+        }
+        return fornecedor;
+    }
+    
+    public static ProdutoRepositorio getProdutoRepositorio(){
+        if(produto == null){
+            String nomeclasse = propriedades.getProperty("ProdutoRepositorio");
+            produto = (ProdutoRepositorio) getInstancia(nomeclasse);
+        }
+        return produto;
+    }
+    
+    public static TransacaoRepositorio getTransacaoRepositorio(){
+        if(transacao == null){
+            String nomeclasse = propriedades.getProperty("TransacaoRepositorio");
+            transacao = (TransacaoRepositorio) getInstancia(nomeclasse);
+        }
+        return transacao;
+    }
+}
+
+
